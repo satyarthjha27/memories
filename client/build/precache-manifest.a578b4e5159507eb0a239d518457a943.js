6ad1461cda7c5c9eeb1d52087a11aac5ec19964c
satyarthjha <satyarth.jha@mybliss.co>
2022-02-02 17:35:05 +0530

    Commit

---
+++ client/build/precache-manifest.a578b4e5159507eb0a239d518457a943.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65072539d6d6e6d955669f6bee24080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "416c9fa81da635de9fe7",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "416c9fa81da635de9fe7",
+    "url": "/static/js/main.8ca313bb.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
